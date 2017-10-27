b3fdf4d5f325f584b96a335d928bbdde5b0c2efd
laraelemans <laraelemans@hotmail.com>
2017-10-27 12:32:35 +0200

    add testing

---
+++ spec/features/user_task_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "User viewing its match" do
+  before {login_as user}
+
+  let!(:user) { create :user, email: "sammy@gmail.com" }
+  it "show his/her match of the day" do
+    visit root_url(user)
+
+    expect(page).to have_text("Your match of today is")
+  end
+end
